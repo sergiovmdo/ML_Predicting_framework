f568f2cbc1011fc00f845ab37f42bcfae2935042
sergiovmdo <sergiovazquezmontesdeoca@gmail.com>
2023-12-01 10:59:47 +0100

    Updated for stratifying on subtypes

--- execute_models.py
+++ execute_models.py
@@ -51,7 +51,7 @@ def get_auc_text_iv(df):
     return f"The AUC for internal validation is: {dict1['auc']} and the correction factor: {dict2['auc']}\n"
 
 
-def print_model(service, model_type, features_iv, features_tt, auc_iv, auc_tt, sample_size):
+def print_model(service, file_id, model_type, features_iv, features_tt, auc_iv, auc_tt, sample_size, subset):
     # asdsad
     service.insert_feature_importances_table(file_id, features_tt, features_iv)
     service.insert_text(file_id, auc_iv)
@@ -60,10 +60,48 @@ def print_model(service, model_type, features_iv, features_tt, auc_iv, auc_tt, s
 
     service.insert_text(file_id, sample_size)
 
+    if subset:
+        model_type = model_type + subset
     service.insert_title(file_id, model_type)
 
+def write_document(data_path, service, file_id, subset=''):
+    for i in range(0, len(file_paths), 2):
+        params_iv = file_paths[i]
+        params_tt = file_paths[i + 1]
 
-data_path = '/home/sergiov/Desktop/full_data_random.csv'
+        model_type = params_iv.split('_')[3]
+        model_type = model_type.split('/')[1]
+
+        # Execution of internal validation
+        sys.argv = [data_path, params_iv]
+        main.main()
+
+        df_iv = pd.read_csv('test.csv')
+
+        features_iv = get_feature_importances_text(df_iv)
+        auc_iv = get_auc_text_iv(df_iv)
+
+        # Execution of train/test
+        sys.argv = [data_path, params_tt]
+        main.main()
+
+        df_tt = pd.read_csv('test.csv')
+
+        features_tt = get_feature_importances_text(df_tt)
+        auc_tt = get_auc_text_tt(df_tt)
+
+        sample_size = get_sample_size(df_tt)
+
+        # Write document
+        print_model(service, file_id, model_type, features_iv, features_tt, auc_iv, auc_tt, sample_size, subset)
+
+
+# Complete data
+complete_path = '/home/sergiov/Desktop/full_data_random.csv'
+
+# Stratified data
+infiltrated_data = '/home/sergiov/Desktop/infiltrated_patients.csv'
+non_infiltrated_data = '/home/sergiov/Desktop/non-infiltrated_patients.csv'
 
 parameters_path = '/home/sergiov/PycharmProjects/ICB_Response_Model/parameter_files'
 file_paths = sorted([os.path.join(root, file) for root, dirs, files in os.walk(parameters_path) for file in files])
@@ -73,32 +111,12 @@ service = GoogleDocsService()
 title = 'Diary 1'
 file_id = service.create_document(title)
 
-for i in range(0, len(file_paths), 2):
-    params_iv = file_paths[i]
-    params_tt = file_paths[i + 1]
+write_document(infiltrated_data, service, file_id, ' Infiltrated')
 
-    model_type = params_iv.split('_')[3]
-    model_type = model_type.split('/')[1]
+write_document(non_infiltrated_data, service, file_id, ' Non-Infiltrated')
 
-    # Execution of internal validation
-    sys.argv = [data_path, params_iv]
-    main.main()
+write_document(complete_path, service, file_id)
 
-    df_iv = pd.read_csv('test.csv')
 
-    features_iv = get_feature_importances_text(df_iv)
-    auc_iv = get_auc_text_iv(df_iv)
 
-    # Execution of train/test
-    sys.argv = [data_path, params_tt]
-    main.main()
 
-    df_tt = pd.read_csv('test.csv')
-
-    features_tt = get_feature_importances_text(df_tt)
-    auc_tt = get_auc_text_tt(df_tt)
-
-    sample_size = get_sample_size(df_tt)
-
-    # Write document
-    print_model(service, model_type, features_iv, features_tt, auc_iv, auc_tt, sample_size)
